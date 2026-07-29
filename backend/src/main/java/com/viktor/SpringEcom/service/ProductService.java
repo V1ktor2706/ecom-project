ff16236fbb3c9ad7227f3031e65cd02805c161db
Viktor Bakalov <viktorbakalov2706@gmail.com>
2026-07-29 17:10:53 +0300

    Add product management methods in ProductService

--- backend/src/main/java/com/viktor/SpringEcom/service/ProductService.java
+++ backend/src/main/java/com/viktor/SpringEcom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.viktor.SpringEcom.model.Product;
 import com.viktor.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -19,4 +21,23 @@ public class ProductService {
     }
 
 
+    public Product getProductById(int id) {
+        return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> searchProduct(String keyword) {
+        return productRepo.searchProducts(keyword);
+    }
 }
