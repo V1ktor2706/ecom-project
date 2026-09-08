f8c9b5fcc1b80342a558b9343d5a04fa455c738d
Viktor Bakalov <vibakalov@tu-sofia.bg>
2026-09-08 15:58:24 +0300

    Added User Controller

---
+++ backend/src/main/java/com/viktor/SpringEcom/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.viktor.SpringEcom.controller;
+
+import com.viktor.SpringEcom.model.User;
+import com.viktor.SpringEcom.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        userService.register(user);
+        return new ResponseEntity<>("User was registered", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        String token = userService.verify(user);
+        return ResponseEntity.ok(token);
+    }
+}
