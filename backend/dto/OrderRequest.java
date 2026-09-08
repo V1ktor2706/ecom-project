9960cc4e9f03e85de030e98ecc27ad577336b68a
Viktor Bakalov <viktorbakalov2706@gmail.com>
2026-09-08 15:56:48 +0300

    Delete backend/dto directory

--- backend/dto/OrderRequest.java
+++
@@ -1,9 +0,0 @@
-package com.viktor.SpringEcom.model.dto;
-
-import java.util.List;
-
-public record OrderRequest(
-        String customerName,
-        String email,
-        List<OrderItemRequest> items
-) {}
