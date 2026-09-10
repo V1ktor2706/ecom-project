a787c2c614615541f86ee57b53411b0e40bcd2a4
Viktor Bakalov <viktorbakalov2706@gmail.com>
2026-09-10 15:18:06 +0300

    Refactor product search method in ProductRepo

--- backend/src/main/java/com/viktor/SpringEcom/repo/ProductRepo.java
+++ backend/src/main/java/com/viktor/SpringEcom/repo/ProductRepo.java
@@ -2,21 +2,17 @@ package com.viktor.SpringEcom.repo;
 
 import com.viktor.SpringEcom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
 
-    @Query("""
-    SELECT p FROM Product p WHERE 
-    LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR 
-    LOWER(p.description) LIKE LOWER(CONCAT('%', :keyword, '%')) OR 
-    LOWER(p.brand) LIKE LOWER(CONCAT('%', :keyword, '%')) OR 
-    LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%'))
-    """)
-    List<Product> searchProducts(String keyword);
+    List<Product> findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseOrBrandContainingIgnoreCaseOrCategoryContainingIgnoreCase(
+            String name,
+            String description,
+            String brand,
+            String category
+    );
 }
