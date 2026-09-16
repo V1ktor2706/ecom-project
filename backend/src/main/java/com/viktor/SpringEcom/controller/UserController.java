a8f83ce90c9af1f0e4875d0352739be1f4ffe1ef
Viktor Bakalov <viktorbakalov2706@gmail.com>
2026-09-16 11:49:37 +0300

    Refactor UserController with comments for clarity
    Added comments for clarity and maintained existing functionality.

--- backend/src/main/java/com/viktor/SpringEcom/controller/UserController.java
+++ backend/src/main/java/com/viktor/SpringEcom/controller/UserController.java
@@ -5,18 +5,16 @@ import com.viktor.SpringEcom.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin//to use the frontend ui
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    //response entity to represent HTTP response
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody User user) {
         userService.register(user);
