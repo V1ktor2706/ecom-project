f7a98aa5b65920a6edc85e763574a1b57ecb7c07
Viktor Bakalov <viktorbakalov2706@gmail.com>
2026-09-16 11:50:02 +0300

    Add email field to User model
    Added email field to User class.

--- backend/src/main/java/com/viktor/SpringEcom/model/User.java
+++ backend/src/main/java/com/viktor/SpringEcom/model/User.java
@@ -11,8 +11,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     private String username;
+    private String email;
     private String password;
     private String role;
 }
